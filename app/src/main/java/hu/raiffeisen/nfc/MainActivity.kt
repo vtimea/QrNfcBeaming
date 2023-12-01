02d959bb814bc110c534dc8f41864d9f5649a521
vajnatimi <vajnatimi@gmail.com>
2023-12-01 12:04:19 +0100

    impl done

--- app/src/main/java/hu/raiffeisen/nfc/MainActivity.kt
+++ app/src/main/java/hu/raiffeisen/nfc/MainActivity.kt
@@ -1,46 +1,64 @@
 package hu.raiffeisen.nfc
 
+import android.content.Intent
+import android.nfc.NfcAdapter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import hu.raiffeisen.nfc.ui.theme.QrNfcBeamingTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
+
+    private var mNfcAdapter: NfcAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             QrNfcBeamingTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Greeting("Android")
+                    var userInput by remember { mutableStateOf("27") }
+
+                    TextField(value = userInput,
+                        onValueChange = { userInput = it },
+                        label = { Text("Type in custom number") })
+
+                    Button(onClick = {
+                        sendMessage(getLink(userInput))
+                    }) {
+                        Text(text = "Set message")
+                    }
                 }
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QrNfcBeamingTheme {
-        Greeting("Android")
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
     }
+
+    private fun sendMessage(text: String) {
+        val intent = Intent(this@MainActivity, MyHostApduService::class.java)
+        intent.putExtra("ndefMessage", text)
+        startService(intent)
+    }
+
+    private fun getLink(customPart: String) =
+        "https://teszt-azonnalifizetes.hu/HCT/3/1//PDV%20BankZRT./C%C3%89GES%20KINGA/HU23120836000021479600100002/HUF539/20231127100731%2B1-0086400/IVPT//4268061937.SUBA.2.87688630.INNOHUH0/POS-RAIFF00015/INV-RAIFF00015/CUSTOMER-RAIFF00015/35346473_IN${customPart}27DOk2pKvODx/https%3A%2F%2Fgiro.RAIFF%2Ftop%2FUBRTHUH0/___/1b02.0WUvYP_USzpi28h4L-CeMeIsN2u8gWylSoa_0eTqHaqPh2zysCsHyDRFTflhGTfJf7ElEertWNqGtBOdbS7VBMsTiilz1Vxmut_FK--UHjEyxhGCwlmQZOlbxisVH_lA"
+
 }
